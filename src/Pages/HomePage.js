5dfa778c3cc744704f1e810bd73fcf8b7a083ae3
Joaquin Ramirez <joaquinramirez97@yahoo.com>
2021-04-12 19:53:22 -0500

    Adde home logo to static images folder

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import homeCoffee from '../StaticImages/home_coffee.png';
 
 function HomePage()
 {
@@ -7,7 +8,7 @@ function HomePage()
         <div className="col-12">
             <div className="col-sm-6 bg-light" style={{"margin": "5% auto", "padding": "5%", "borderRadius": "10px",}}>
                 <h1>Build Invoices For Free!</h1>
-                <img style={{"margin": "30px 0", "width": "50%", "height": "auto"}} alt="icon" src={"https://banner.uclipart.com/20200112/eos/coffee-cup-coffee-cup.png"}/>
+                <img style={{"margin": "30px 0", "width": "50%", "height": "auto"}} alt="icon" src={homeCoffee} />
                 <div className="row">
                     <div className="col-6">
                         <Link className="btn btn-primary col-12" to="/Register">Register</Link>
